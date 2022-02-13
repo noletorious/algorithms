c0e932139910daa035d6eaf99e81e4b0e8729434
noletorious <naatorres@gmail.com>
2022-02-13 10:15:54 -0800

    Update addTwoNumbers

--- addTwoNumbers.js
+++ addTwoNumbers.js
@@ -3,7 +3,9 @@ function ListNode(val, next) {
   this.next = next === undefined ? null : next;
 }
 
-const a = new ListNode("hello");
+// You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+// You may assume the two numbers do not contain any leading zero, except the number 0 itself.
 
 // input: l1 = [2,4,3], l2 = [5,6,4]
 // output: [7,0,8]
@@ -24,16 +26,29 @@ const a = new ListNode("hello");
 // join back to string and convert to number
 // }
 
-const largeArray = Array(2).fill(1);
-const largeArray2 = Array(4).fill(4);
+// const largeArray = Array(2).fill(1);
+// const largeArray2 = Array(4).fill(4);
 
-const largeArrSplit = largeArray.join("555");
-const large2ArrSplit = largeArray2.join("");
+// const largeArrSplit = largeArray.join("555");
+// const large2ArrSplit = largeArray2.join("");
 
 // console.log(Number(largeArrSplit) + Number(large2ArrSplit));
-console.log(largeArrSplit);
 
 // scalable approach: linked list
 // edge case #1: What if l1.length !=== l2.length
 
+const a = new ListNode("hello");
+const l1 = [2, 4, 4];
+//          x
+const l2 = [5, 6, 7];
+//          y
+// carry = 0
+
 function addTwoNumbers(l1, l2) {}
+
+const atnpre = document.getElementById("atnpre");
+atnpre.innerHTML += addTwoNumbers;
+const atninput = document.getElementById("atninput");
+atninput.innerHTML += "[" + l1 + "], [" + l2 + "]";
+const atnoutput = document.getElementById("atnoutput");
+atnoutput.innerHTML += JSON.stringify(addTwoNumbers(l1, l2));
