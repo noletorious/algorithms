16864a7d4b33d8871b707e396d9c1b2003cd8769
noletorious <naatorres@gmail.com>
2022-03-10 11:39:58 -0800

    Update moveZerosToEnd and reverseWords

--- moveZerosToEnd.js
+++ moveZerosToEnd.js
@@ -3,47 +3,140 @@
 input:  arr = [1, 10, 0, 2, 8, 3, 0, 0, 6, 4, 0, 5, 7, 0]
 output: [1, 10, 2, 8, 3, 6, 4, 5, 7, 0, 0, 0, 0, 0]
 
-[], -> []
-[2], -> [2]
-[0,2] -> [2,0]
-[undefined, 9], -> all items are integers
-
 - preserve relative order of non zeros
-- do I need to return the original array manipluted? -> Yes
-zerosCount = 0
+- we are ultimately returning the original array
+
+- [], -> []
+- [2], -> [2]
+- [0,2] -> [2,0]
+- [undefined, 9], -> all items are integers
 
 */
 
-/** SOLUTION
+/** BRUTE SOLUTION, the purely iterative approach
+ * 
+ * 	0, 10, 0, 8, 3, 0
+ *  i
+ *  0,  0, 0, 0, 0, 0
+ *  j
+ * 
 
-	zeros = 0
+	T:O(n) // O(n^2)
+	S:O(1) // O(n)
+
+	arrLen = arr.length
+
+	nonZerosNums = new Array(arr.legnth)
+	zeros = 3
+
+	loop over arr // 0(n)
+		push the non zero nums to nonZerosNums array
+		change current number to 0
+
+	loop over nonZerosNum, i // 0(n)
+	   loop over arr, j // 0(n)
+	    if nonZerosNum[i] !== undefined, 
+	   		j = i
+		else 
+			arr[i]=0
+		i++
+		j++
+*/
+
+/** OPTIMIZED SOLUTION, the shifting pointers approach
+ * 
+ *  0, 0, 10, 8, 3, 0
+ *  10, 8, 3, 8, 3, 0
+ *               p1
+ *                    p2
+ * 
+ *  T: O(n^2)
+ *  S: O(1)
+
+	arrLen = arr.length
 	p1 = 0
-	p2 = 0
-	arrayLen = array.length;
-           2       10
-	while(p1 < arrayLen){
-		if(arr[p1] !== 0){
+	p2 = 2
+	zeros = 1
+          0      6
+	while(p2 < arrLen){
+		if(p1 !== 0){
 			p1++
-		}else{
-			zeros++
+		} else {
+			zero++
 			p2 = p1 + 1
-			     0
-			let temp = arr[p2];
-			if(temp !== 0){
-				arr[p1]=temp;
+			while(p1 < p2){
+				if(p2 !== 0){
+					p1 = p2
+					p1++
+					p2++
+				} else {
+					zeros++
+					p2++
+				}
 			}
-			p2++
 		}
 	}
+	return arr
 
-*/
+ */
 
-const arr = [1, 10, 0, 0, 0, 2, 8, 3, 7, 0];
-//                 p1
-//                     p2
+/** OPTIMIZED 
+ * 
+ *  T: O(n)
+ *  S: O(1)
+ * 
+ *   r
+ * 1,-1,2,4,6
+ *   l
+ * 
+	const arrLen = arr.length;
 
-function moveZerosToEnd(array) {
-  return array;
+	let left = 0,
+		right = 0,
+		nextNonZero = 0;
+
+	while (right < arrLen) {
+		if (arr[right] !== 0) {
+			nextNonZero = arr[right];
+			arr[left] = nextNonZero;
+			if (right > left) {
+				arr[right] = 0;
+			}
+			right++;
+			left++;
+		} else {
+			right++;
+		}
+	}
+	return arr;
+
+ */
+
+const array = [1, 10, 0, 2, 8, 3, 0, 0, 6, 4, 0, 5, 7, 0];
+//                                       p1
+//                                 p2
+
+function moveZerosToEnd(arr) {
+  const arrLen = arr.length;
+
+  let left = 0,
+    right = 0,
+    nextNonZero = 0;
+
+  while (right < arrLen) {
+    if (arr[right] !== 0) {
+      nextNonZero = arr[right];
+      arr[left] = nextNonZero;
+      if (right > left) {
+        arr[right] = 0;
+      }
+      right++;
+      left++;
+    } else {
+      right++;
+    }
+  }
+  return arr;
 }
 
-console.warn(moveZerosToEnd(arr));
+console.log(moveZerosToEnd(array));
--- reverseWords.js
+++ reverseWords.js
@@ -1,71 +1,52 @@
-arr = [
-  "p",
-  "e",
-  "r",
-  "f",
-  "e",
-  "c",
-  "t",
-  "  ",
-  "m",
-  "a",
-  "k",
-  "e",
-  "s",
-  "  ",
-  "p",
-  "r",
-  "a",
-  "c",
-  "t",
-  "i",
-  "c",
-  "e",
-];
+let sentence = ["h", "e", "y", "  ", "t", "h", "e", "r", "e", "  ", "y", "o", "u"];
+// output -> ["y", "o", "u","  ", "t", "h", "e", "r", "e", "  ","h", "e", "y" ]
 
-/** RETURNING
- *  arr = [' ','h','e','y',' ','y','o','u']
- *              p1
- *         				p2
- *  result = ['y','o','u',' ','h','e','y',]
+/** QUESTIONS AND CONSTRAINTS
+ * 
+ *  - [" ", "h", ...], starts with a space? -> trims beginning and end if they are spaces
+ *  - Are we returning the original array? 
+ *  - [" ", " "], array with just spaces? -> 
+ * 
  */
 
-/** CONTRAINTS
- *  Return the original array?
- *  Best case: Return reversed ordered array
- *  Worst case: null
- *  [], empty array -> Will always have items in array
- *  ['e'], one item -> ['e']
- *  [' ', 'h', 'e', 'y'], beginning with space -> ['h', 'e', 'y', ']
- *  ['words',' ', ' ', 'some'], multiple space -> ['some', ' ', 'words']
+/** BRUTE SOLUTION
+ * 
+ *  T: O(n^2)
+ *  S: O(n^2)
+ * 
+ *  Concept: output = [...endWord," ",output]
+  
+  1. Create variables, mainly the output array
+  2. Loop thought the sentence array from the end
+    - Push each letter to a new array, essentially reversing and copying the original array
+  3. Loop through array, 
+    - Create a new array that will hold the word
+    - While looping array, 
+      - until check if space, create word
+    - Update the output with new word, output = [...endWord," ",output]
+  4. return output
+  
  */
 
-/** BRUTE PSUEDO SOLUTION
- *  S: O(N), T: O(N), Introduce a new data structure based on input
- *  ['y', 'o', 'u', ' ', 'h', 'e', 'y',]
- *  Loop through array in reverse
- *  Per iteration, push the item into a new array so it's overall structure is reversed
- */
-/** OPTIMIZED PSUEDO SOLUTION
- *  S: O(1),
- *  T: O(N),
- *  - Helper function that will reverse words at start and stop position
- *  - Because we're retuning the original array, mutate original array
- */
+/** OPTIMIZED SOLUTION, shifting pointer method
+ * 
+ *  T: O(n)
+ *  S: O(1)
+ * 
 
-const reverseWords = (array) => {
-  const result = [];
+  1. Write a swappingEndsToCenter helper function, swapEnds(arr, start, end)
+  2. Use the swapEnds functions to swap the array once, now the words in the right position
+  3. If first element in array is space, shift
+  4. If last element in array is space, pop
+  3. Loop through the array in order to swap the words in the correct order
+      - If p1 === " ", using two pointers,
+      - swap the words at coordnates start = p2, end = p1 - 1 using the helper function
+      - p2 = p1 + 1, p1++
+    -p++
+  4. return arr
 
-  // Loop through array push a word into the array when a word is created
-  for (let i = array.length; i > 0; i--) {
-    if (array[i] !== "  " || array[i] === " ") {
-      result.push(" ");
-    } else if (array[i] === "  ") {
-      result.push(" ");
-    }
-  }
+*/
 
-  return result;
-};
+const reverseWords = (array) => {};
 
 console.log(reverseWords(arr));
