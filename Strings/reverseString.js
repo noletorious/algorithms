436c2eb22923e0a2541989974da9f85e506875d7
Noel Torres <naatorres@gmail.com>
2022-04-02 16:03:03 -0700

    Update reverseString and helpers

---
+++ Strings/reverseString.js
@@ -0,0 +1,62 @@
+/** Reverse strings
+ *
+ * 1. Iteratively
+ * 2. Using built-in js methods
+ * 3. Using built-in js methods
+ * 4. Recursively, T: O(2^n), S:O(n)
+ * 5. Recursively, T: O(2^n), S:O(n)
+ *
+ */
+
+const my_string = "Hello, my name is Noel";
+
+// S: O(n)
+// T: O(n)
+function reverseString1(str) {
+  // an empty array to hold the newly ordered array
+  const reversed = [];
+  // get the legnth of the string and minus 1 to get the end position
+  const stringLength = str.length - 1;
+  // loop through the array
+  for (i = stringLength; i >= 0; i--) {
+    reversed.push(str[i]);
+  }
+  const output = reversed.join("");
+  return output;
+}
+
+// S:O(n)
+// T:O(?)
+function reverseString2(str) {
+  return str.split("").reverse().join("");
+}
+
+function reverseString3(str) {
+  return [...str].reverse().join("");
+}
+
+function reverseString4(str) {
+  let strArray = str.split("");
+  let reversedArray = [];
+
+  function addToArrayFromEnd(arr) {
+    if (arr.length > 0) {
+      reversedArray.push(arr.pop());
+      addToArrayFromEnd(arr);
+    }
+  }
+
+  addToArrayFromEnd(strArray);
+
+  return reversedArray.join("");
+}
+
+function reverseString5(str) {
+  if (str <= 1) {
+    return str;
+  } else {
+    return reverseString5(str.substr(1)) + str.charAt(0);
+  }
+}
+
+console.log(reverseString5(my_string));
