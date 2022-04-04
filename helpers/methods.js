c6aefd2d2fb3fa045cc41f2417526335fcc9904b
noletorious <naatorres@gmail.com>
2022-04-04 14:05:25 -0700

    Update LinkedList self tutorial

--- helpers/methods.js
+++ helpers/methods.js
@@ -1,16 +1,17 @@
 const array = [94, 2, 44, 24, 9, 1, -1];
 
-/** ARRAYS (and strings) */
+/** ARRAYS (and strings) 
 
-// Shift -> removes first element, returns that element
-// Unshift -> adds one or more elements to beginning of array, returns new array legnth
+Shift -> removes first element, returns that element
+Unshift -> adds one or more elements to beginning of array, returns new array legnth
 
-// Slice -> returns a shallow copy of an array, with start and end parameters. Start is inclusive, end is exclusive.
-// Splice ->  changes the contents of an array by removing or replacing existing elements and/or adding new elements in place.
+Slice -> returns a shallow copy of an array, with start and end parameters. Start is inclusive, end is exclusive.
+Splice ->  changes the contents of an array by removing or replacing existing elements and/or adding new elements in place.
 
-// Push -> adds one or more elements to end of array, returns new length of array.
-// Pop -> removes that last element of an array, returns that element
+Push -> adds one or more elements to end of array, returns new length of array.
+Pop -> removes that last element of an array, returns that element
 
+*/
 const str = "The quick brown fox jumps over the lazy dog.";
 
 /** STRINGS */
@@ -26,3 +27,4 @@ const str = "The quick brown fox jumps over the lazy dog.";
 /** OBJECTS */
 
 // MAP -> Like an object and an array had a baby
+// SET -> Like an object and an array had a baby
